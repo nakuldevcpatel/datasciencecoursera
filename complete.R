7882d95f42b9faa6a4f95245ea0fdb951354acab
Nakuldev <nakuldevcpatel@gmail.com>
2015-06-18 19:51:41 +0530

    Assignment - 1

---
+++ complete.R
@@ -0,0 +1,22 @@
+complete <- function(directory, id = 1:332) {
+        ## 'directory' is a character vector of length 1 indicating
+        ## the location of the CSV files
+        
+        ## 'id' is an integer vector indicating the monitor ID numbers
+        ## to be used
+        
+        ## Return a data frame of the form:
+        ## id nobs
+        ## 1  117
+        ## 2  1041
+        ## ...
+        ## where 'id' is the monitor ID number and 'nobs' is the
+        ## number of complete cases
+        
+        files <- (Sys.glob("specdata//*.csv"))[id]
+        
+        nobs   <- sapply(files,
+                         function(file) sum(complete.cases(read.csv(file))))
+        
+        return (data.frame(id,nobs))
+}
